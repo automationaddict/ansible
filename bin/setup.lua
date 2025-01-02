8b8674175516ba5fd1974a2e1682cc68fd78a226
John Nelson <automationaddict@me.com>
2025-01-01 20:38:07 -0500

    Fix tmp dir not being created

--- bin/setup.lua
+++ bin/setup.lua
@@ -1,40 +1,32 @@
 local REPO_URL = "https://github.com/automationaddict/ansible.git"
 
+-- Create the tmp directory
+local tmp_dir = "./tmp" -- Directory path
+
+print("Check if the directory exists and remove it")
+if os.execute("test -d " .. tmp_dir) then
+    -- Remove the existing directory
+    os.execute("rm -rf " .. tmp_dir)
+end
+
+print("Create the directory")
+if os.execute("mkdir -p " .. tmp_dir) then
+    print(tmp_dir .. " directory has been created.")
+end
+
 -- Function to run a shell command and check its result
 local function run_command(command)
     local output_file = "./tmp/setup_log.txt"
     local ok, exit_type, exit_code = os.execute(command .. " >> " .. output_file .. " 2>&1")
-    print("Ok:", ok)
-    print("Exit Type:", exit_type)
-    print("Exit Code:", exit_code)
+    -- print("Ok:", ok)
+    -- print("Exit Type:", exit_type)
+    -- print("Exit Code:", exit_code)
     if exit_code == 0 then
         return true
     end
     print("Command failed: " .. command)
     print("Check the log at " .. output_file .. " for details")
     return false
-end
-
--- Create the tmp directory
-local function create_tmp_directory()
-    local tmp_dir = "./tmp" -- Directory path
-
-    -- Check if the directory exists and remove it
-    if run_command("test -d " .. tmp_dir) then
-        -- Remove the existing directory
-        if not run_command("rm -rf " .. tmp_dir) then
-            print("An error occurred while removing the tmp directory.")
-            os.exit(1)
-        end
-    end
-
-    -- Create the directory
-    if run_command("mkdir -p " .. tmp_dir) then
-        print(tmp_dir .. " directory has been created.")
-    else
-        print("An error occurred while creating the tmp directory.")
-        os.exit(1)
-    end
 end
 
 print("Updating, Upgrading, and Cleaning up the system")
